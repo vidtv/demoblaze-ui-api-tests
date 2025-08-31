9435d448b1818a64330533d61dc679cfef852c9f
vidtv <nikit1534@gmail.com>
2025-08-31 23:08:30 +0200

    Added org.json library and AuthIntegrationTests.java test class

---
+++ src/test/java/integration/AuthIntegrationTests.java
@@ -0,0 +1,16 @@
+package integration;
+
+import base.BaseTest;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import org.junit.jupiter.api.Test;
+
+@Epic("Demoblaze")
+@Feature("User Authentication and Registration")
+public class AuthIntegrationTests extends BaseTest {
+
+    @Test
+    void shouldCreateNewUserViaUiAndVerifyInApi() {
+
+    }
+}
